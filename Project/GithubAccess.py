a093b4115ec54f4bca5e57ab57cb80a3b0e8e4c9
vincecasey <vincecasey@users.noreply.github.com>
2021-01-09 23:38:31 +0000

    Removing Leftover Commented Out Code

--- Project/GithubAccess.py
+++ Project/GithubAccess.py
@@ -10,7 +10,6 @@ import pandas as pd
 from bokeh.palettes import Category20c
 from bokeh.transform import cumsum
 from github import Github
-
 from datetime import datetime as dt
 #from bokeh.charts import show
 from bokeh.models import DatetimeTickFormatter
@@ -141,65 +140,3 @@ class CommitDate:
 #
 
 display(lang, picture, username)
-
-
-
-#
-#Bokeh Graph Testing
-#
-
-# # prepare some data
-# x = [1, 2, 3, 4, 5]
-# y = [6, 7, 2, 4, 5]
-
-# # output to static HTML file
-# output_file("lines.html")
-
-# # create a new plot with a title and axis labels
-# p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
-
-# # add a line renderer with legend and line thickness
-# p.line(x, y, legend_label="Temp.", line_width=2)
-
-# # show the results
-# show(p)
-
-# output_file('vbar.html')
-
-# p = figure(plot_width=400, plot_height=400)
-# p.vbar(x=[1, 2, 3], width=0.5, bottom=0,
-#        top=[1.2, 2.5, 3.7], color="firebrick")
-
-# show(p)
-
-
-
-
-
-# def get_repos(username):
-#   res = requests.get('https://api.github.com/users/' + username + '/repos')
-#   return res.json()
-
-
-
-# def project_sum(username):
-#   data = get_repos(username)
-#   total_data_size = 0
-
-#   for j in data:
-#     if j['size']:
-#       total_data_size += j['size']
-#   return total_data_size
-
-
-# data=(json.dumps(get_repos('caseyvtcd'), indent = 5))
-# print(project_sum("caseyvtcd"))
-
-
-# data = [project_sum("caseyvtcd")]
-# X = np.arange(4)
-# fig = plt.figure()
-# ax = fig.add_axes([0,0,1,1])
-# ax.bar(X + 0.00, data[0], color = 'b', width = 0.25)
-
-# plt.show()
